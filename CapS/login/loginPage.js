0ea78d2a9ccc1840383bc965313001c4632c1894
ieeseul <72555387+ieeseul@users.noreply.github.com>
2022-10-24 01:04:13 +0900

    Add files via upload

--- CapS/login/loginPage.js
+++ CapS/login/loginPage.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-    document.getElementById("btn5").onclick = function(){
-        // 대충 얼굴인증 안햇으면 얼굴인증 먼저하라는 함수
+    document.getElementById("login").onclick = function(){
+        
     }
 }
 
